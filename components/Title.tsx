7f92abb3fe5ead9ed0099d1b97a6dce728119a36
Tomasz Sikora <tomasz_sikora@epam.com>
2022-08-18 08:56:15 +0200

    children typescript

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { TitleStyles } from "./styles/Title.styled";
-
-export default function Title({ children }) {
+interface children {
+    children: React.ReactNode;
+}
+export default function Title({ children }: children) {
     return <TitleStyles>{children}</TitleStyles>;
 }
