13590d70e5e570f2d42b0166d159caa30ed37880
Tomasz Sikora <tomasz_sikora@epam.com>
2022-08-18 08:44:11 +0200

    title component added

---
+++ components/Title.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { TitleStyles } from "./styles/Title.styled";
+
+export default function Title({ children }) {
+    return <TitleStyles>{children}</TitleStyles>;
+}
--- pages/favourites.tsx
+++ pages/favourites.tsx
@@ -5,35 +5,36 @@ import LoadingSpinner from "../components/LoadingSpinner";
 import PokemonList from "../components/PokemonList";
 import { PokemonFavContext } from "../contexts/PokemonFavContext";
 import { TitleStyles } from "../components/styles/Favourite.styled";
+import Title from "../components/Title";
 
 const POKEMON_FAV_QUERY = gql`
-  query POKEMON_FAV_QUERY($pokemonArr: [Int!]) {
-    pokemons: pokemon_v2_pokemon(where: { id: { _in: $pokemonArr } }) {
-      name
-      id
+    query POKEMON_FAV_QUERY($pokemonArr: [Int!]) {
+        pokemons: pokemon_v2_pokemon(where: { id: { _in: $pokemonArr } }) {
+            name
+            id
+        }
     }
-  }
 `;
 export default function Favourites() {
-  const FavContext = useContext(PokemonFavContext);
-  const { pokemonFavList }: any = FavContext;
-  const { data, error, loading } = useQuery(POKEMON_FAV_QUERY, {
-    variables: {
-      pokemonArr: pokemonFavList,
-    },
-  });
+    const FavContext = useContext(PokemonFavContext);
+    const { pokemonFavList }: any = FavContext;
+    const { data, error, loading } = useQuery(POKEMON_FAV_QUERY, {
+        variables: {
+            pokemonArr: pokemonFavList,
+        },
+    });
 
-  if (loading) return <LoadingSpinner />;
-  if (error) return <p>Error :(</p>;
+    if (loading) return <LoadingSpinner />;
+    if (error) return <p>Error :(</p>;
 
-  return (
-    <div>
-      <TitleStyles>Favourite list</TitleStyles>
-      {pokemonFavList.length ? (
-        <PokemonList data={data.pokemons} />
-      ) : (
-        <FavouriteEmptyPage />
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <Title>Favourite list</Title>
+            {pokemonFavList.length ? (
+                <PokemonList data={data.pokemons} />
+            ) : (
+                <FavouriteEmptyPage />
+            )}
+        </div>
+    );
 }
